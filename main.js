49d3592e1cf9a121ba385f9f121afe858febf625
Diyorbek <163130402+diorboi77@users.noreply.github.com>
2024-04-07 16:06:54 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+//alert('hello')
+//console.log('hello');
+//var a = 1; eski o'zgaruvchining ismi
+//let a = 1; yangi o'zgaruvchi nomi
+//let x = 5
+//let y = 7
+//console.log(x / y)
+//console.log(x + y)
+//console.log(x - y)
+//console.log(x * y)
+//x = 10
+//console.log(x)
+//let yosh = prompt('Yoshingiz nechchida', 18);
+//alert(` Sizning yoshingiz ${yosh}` );
+//let a = 1;
+//let b = 'hello';
+//let d = true;
+//let c = false;
+//console.log(true);
+//console.log(false);
+//console.log(typeof a)
+//console.log(typeof b)
+//let a = 3
+//let p = 3.14
+//console.log(2*p*3)
+//let a = 10
+//console.log(a)
+// a += 5
+// let b = 1
+// console.log(b)
+// b -= 1
+// console.log(a);
+// console.log(b);
+// console.log(10-5-2);
+// console.log(8*2)
+// let a = prompt('A ga son bering');
+// let b = prompt('B ga son bering');
+// console.log(+a + +b);
+// console.log(a-b);
+// console.log(a*b);
+// console.log(a/b);
